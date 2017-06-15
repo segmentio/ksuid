bb9a00e8bcf88bafe1ed8c57398d5e7d51b79ab0
Achille Roussel <achille@segment.com>
2017-06-14 17:30:32 -0700

    fix typo

--- ksuid.go
+++ ksuid.go
@@ -230,7 +230,7 @@ func New() KSUID {
 // Generates a new KSUID
 func NewRandom() (ksuid KSUID, err error) {
 	// Go's default random number generators are not safe for concurrent use by
-	// multiple goroutines so use of the rander and randBuffer are explicitly
+	// multiple goroutines, the use of the rander and randBuffer are explicitly
 	// synchronized here.
 	randMutex.Lock()
 
