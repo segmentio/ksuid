8f02cd8eaa8b00b863521aa4755d3af2e8fb3e68
Olivier Melcher <olivier.melcher@gmail.com>
2017-07-22 17:03:49 -0500

    support nil value in sql

--- ksuid_test.go
+++ ksuid_test.go
@@ -187,6 +187,14 @@ func TestSqlValuer(t *testing.T) {
 	}
 }
 
+func TestSqlValuerNilValue(t *testing.T) {
+	if v, err := Nil.Value(); err != nil {
+		t.Error(err)
+	} else if v != nil {
+		t.Error("bad nil value")
+	}
+}
+
 func TestSqlScanner(t *testing.T) {
 	id1 := New()
 	id2 := New()
