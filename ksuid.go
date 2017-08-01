c98a6be9fd2278eac23e52b8cdd238bb3f00f065
Wendy Hu <wenting.hu@uwaterloo.ca>
2017-08-01 13:17:33 -0700

    Add func to create ksuid from passed-in time

--- ksuid.go
+++ ksuid.go
@@ -195,8 +195,7 @@ func New() KSUID {
 	return ksuid
 }
 
-// Generates a new KSUID
-func NewRandom() (ksuid KSUID, err error) {
+func setRandBuffer() (ksuid KSUID, err error) {
 	// Go's default random number generators are not safe for concurrent use by
 	// multiple goroutines, the use of the rander and randBuffer are explicitly
 	// synchronized here.
@@ -206,13 +205,30 @@ func NewRandom() (ksuid KSUID, err error) {
 	copy(ksuid[timestampLengthInBytes:], randBuffer[:])
 
 	randMutex.Unlock()
+	return
+}
 
+// Generates a new KSUID
+func NewRandom() (ksuid KSUID, err error) {
+	ksuid, err = setRandBuffer()
 	if err != nil {
 		ksuid = Nil // don't leak random bytes on error
 		return
 	}
 
 	ts := timeToCorrectedUTCTimestamp(time.Now())
+	binary.BigEndian.PutUint32(ksuid[:timestampLengthInBytes], ts)
+	return
+}
+
+func NewRandomWithTime(t time.Time) (ksuid KSUID, err error) {
+	ksuid, err = setRandBuffer()
+	if err != nil {
+		ksuid = Nil // don't leak random bytes on error
+		return
+	}
+
+	ts := timeToCorrectedUTCTimestamp(t)
 	binary.BigEndian.PutUint32(ksuid[:timestampLengthInBytes], ts)
 	return
 }
