d24e51dda38d4a3994a500616c71cd36ec385889
Sergey Polichnoy <pilatuz@gmail.com>
2021-10-29 18:30:28 +0300

    a few minor updates to uint128 (#53)

--- uint128.go
+++ uint128.go
@@ -1,6 +1,10 @@
 package ksuid
 
-import "fmt"
+import (
+	"encoding/binary"
+	"fmt"
+	"math/bits"
+)
 
 // uint128 represents an unsigned 128 bits little endian integer.
 type uint128 [2]uint64
@@ -15,86 +19,27 @@ func makeUint128(high uint64, low uint64) uint128 {
 
 func makeUint128FromPayload(payload []byte) uint128 {
 	return uint128{
-		// low
-		uint64(payload[8])<<56 |
-			uint64(payload[9])<<48 |
-			uint64(payload[10])<<40 |
-			uint64(payload[11])<<32 |
-			uint64(payload[12])<<24 |
-			uint64(payload[13])<<16 |
-			uint64(payload[14])<<8 |
-			uint64(payload[15]),
-		// high
-		uint64(payload[0])<<56 |
-			uint64(payload[1])<<48 |
-			uint64(payload[2])<<40 |
-			uint64(payload[3])<<32 |
-			uint64(payload[4])<<24 |
-			uint64(payload[5])<<16 |
-			uint64(payload[6])<<8 |
-			uint64(payload[7]),
+		binary.BigEndian.Uint64(payload[8:]), // low
+		binary.BigEndian.Uint64(payload[:8]), // high
 	}
 }
 
-func (v uint128) ksuid(timestamp uint32) KSUID {
-	return KSUID{
-		// time
-		byte(timestamp >> 24),
-		byte(timestamp >> 16),
-		byte(timestamp >> 8),
-		byte(timestamp),
-
-		// high
-		byte(v[1] >> 56),
-		byte(v[1] >> 48),
-		byte(v[1] >> 40),
-		byte(v[1] >> 32),
-		byte(v[1] >> 24),
-		byte(v[1] >> 16),
-		byte(v[1] >> 8),
-		byte(v[1]),
-
-		// low
-		byte(v[0] >> 56),
-		byte(v[0] >> 48),
-		byte(v[0] >> 40),
-		byte(v[0] >> 32),
-		byte(v[0] >> 24),
-		byte(v[0] >> 16),
-		byte(v[0] >> 8),
-		byte(v[0]),
-	}
+func (v uint128) ksuid(timestamp uint32) (out KSUID) {
+	binary.BigEndian.PutUint32(out[:4], timestamp) // time
+	binary.BigEndian.PutUint64(out[4:12], v[1])    // high
+	binary.BigEndian.PutUint64(out[12:], v[0])     // low
+	return
 }
 
-func (v uint128) bytes() [16]byte {
-	return [16]byte{
-		// high
-		byte(v[1] >> 56),
-		byte(v[1] >> 48),
-		byte(v[1] >> 40),
-		byte(v[1] >> 32),
-		byte(v[1] >> 24),
-		byte(v[1] >> 16),
-		byte(v[1] >> 8),
-		byte(v[1]),
-
-		// low
-		byte(v[0] >> 56),
-		byte(v[0] >> 48),
-		byte(v[0] >> 40),
-		byte(v[0] >> 32),
-		byte(v[0] >> 24),
-		byte(v[0] >> 16),
-		byte(v[0] >> 8),
-		byte(v[0]),
-	}
+func (v uint128) bytes() (out [16]byte) {
+	binary.BigEndian.PutUint64(out[:8], v[1])
+	binary.BigEndian.PutUint64(out[8:], v[0])
+	return
 }
 
 func (v uint128) String() string {
 	return fmt.Sprintf("0x%016X%016X", v[0], v[1])
 }
-
-const wordBitSize = 64
 
 func cmp128(x, y uint128) int {
 	if x[1] < y[1] {
@@ -113,29 +58,22 @@ func cmp128(x, y uint128) int {
 }
 
 func add128(x, y uint128) (z uint128) {
-	x0 := x[0]
-	y0 := y[0]
-	z0 := x0 + y0
-	z[0] = z0
-
-	c := (x0&y0 | (x0|y0)&^z0) >> (wordBitSize - 1)
-
-	z[1] = x[1] + y[1] + c
+	var c uint64
+	z[0], c = bits.Add64(x[0], y[0], 0)
+	z[1], _ = bits.Add64(x[1], y[1], c)
 	return
 }
 
 func sub128(x, y uint128) (z uint128) {
-	x0 := x[0]
-	y0 := y[0]
-	z0 := x0 - y0
-	z[0] = z0
-
-	c := (y0&^x0 | (y0|^x0)&z0) >> (wordBitSize - 1)
-
-	z[1] = x[1] - y[1] - c
+	var b uint64
+	z[0], b = bits.Sub64(x[0], y[0], 0)
+	z[1], _ = bits.Sub64(x[1], y[1], b)
 	return
 }
 
-func incr128(x uint128) uint128 {
-	return add128(x, uint128{1, 0})
+func incr128(x uint128) (z uint128) {
+	var c uint64
+	z[0], c = bits.Add64(x[0], 1, 0)
+	z[1] = x[1] + c
+	return
 }
